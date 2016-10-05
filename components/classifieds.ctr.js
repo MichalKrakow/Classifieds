693b3aef7dd3943149800e1398108a8dcc75cbfe
MichalKrakow <michal@ikropka.com>
2016-10-05 20:56:09 +0200

    All good - work in progress lesson 21

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,14 +4,56 @@
 
 	angular
 		.module("ngClassifieds") // using module - no brackets
-		.controller("classifiedsCtrl",function($scope){
+		.controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
 			
-			$scope.name = {
-				first:"Zbyszko",
-				last:"zbogdanca"
+			classifiedsFactory.getClassifieds().then(function(classifieds){
+				$scope.classifieds = classifieds.data; 
+			});
+
+			$scope.closeSidenav = function(){
+				$mdSidenav('left').close();
 			};
-			
-			$scope.message = "siemka";
+
+			$scope.openSidenav = function(){
+				$mdSidenav('left').open();
+			};
+
+			$scope.saveClassified = function(classified){
+				if(classified){
+					$scope.classifieds.unshift(classified);
+					$scope.classified = {};
+					$scope.closeSidenav();
+					showToast("classified saved");
+				}
+			};
+
+			$scope.saveEdit = function(classified){
+				$scope.isEdit = false;
+				$scope.classified = {};
+				showToast("classified edited");
+				$scope.closeSidenav(); 
+			};
+
+			$scope.newClassified = function(){
+				$scope.classified = {};
+				$scope.isEdit = false;
+				$scope.openSidenav();
+			};
+
+			$scope.editClassified = function(classified){
+				$scope.isEdit = true;
+				$scope.classified = classified;
+				$scope.openSidenav();
+			};
+
+			function showToast(message){
+				$mdToast.show( 
+					$mdToast.simple()
+						.content(message)
+						.position('top, right')
+						.hideDelay(3000)
+				);
+			}
 
 		});
 
