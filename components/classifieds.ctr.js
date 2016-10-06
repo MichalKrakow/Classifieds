38b0bceda4b1a1799f12de83f5ffd2e4f143f617
MichalKrakow <michal@ikropka.com>
2016-10-06 23:54:31 +0200

    czwartek wieczor koniec dnia

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,10 +4,11 @@
 
 	angular
 		.module("ngClassifieds") // using module - no brackets
-		.controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
-			
+		.controller("classifiedsCtrl",function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog){
+
 			classifiedsFactory.getClassifieds().then(function(classifieds){
-				$scope.classifieds = classifieds.data; 
+				$scope.classifieds = classifieds.data;
+				$scope.categories = getCategories(classifieds.data);
 			});
 
 			$scope.closeSidenav = function(){
@@ -46,6 +47,26 @@
 				$scope.openSidenav();
 			};
 
+			$scope.deleteClassified = function(event, classified){
+				var confirm = $mdDialog.confirm()
+					.title('czy na pewno usunąć: ' + classified.title + '?')
+					.ok('tak, usuń')
+					.cancel('nie teraz')
+					.targetEvent(event);
+
+				$mdDialog.show(confirm).then(function(){
+					var index = $scope.classifieds.indexOf(classified);
+					$scope.classifieds.splice(index,1); // usun jeden i przesun
+				},function(){ // then - else
+
+				});
+			}
+
+			$scope.clearFilters = function(){
+				$scope.category = '';
+				$scope.classifiedsFilter = '';
+			};
+
 			function showToast(message){
 				$mdToast.show( 
 					$mdToast.simple()
@@ -53,6 +74,19 @@
 						.position('top, right')
 						.hideDelay(3000)
 				);
+			}
+
+
+
+			function getCategories(classifieds){
+				var categories = [];
+				angular.forEach(classifieds,function(classified){
+					angular.forEach(classified.categories, function(category){
+						if(categories.indexOf(category) < 0)
+							categories.push(category);
+					});
+				});
+				return categories;
 			}
 
 		});
